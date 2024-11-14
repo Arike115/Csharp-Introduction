f47d89a4f4e2ee48f2ca8d07e6007e08c290d3d8
Arike115 <ajibolaoluwafunke669@gmail.com>
2024-11-14 17:01:47 +0100

    linq

--- Csharp-Introduction/Program.cs
+++ Csharp-Introduction/Program.cs
@@ -1,19 +1,31 @@
-﻿using Csharp_Introduction;
-using System.Text;
+﻿//linq in c#
+//language integrated query
+//Types of Operators
+//filtering
+//projecting
+//ordering
+//setoperators
+//elementoperators
 
-Student[] stud = new Student[3];
+using Csharp_Introduction;
 
-stud[0] = new Student();
-stud[1] = new Studentinfo();
-stud[2] = new StudentList();
+QueryFunctions.OrderingMethod();
+//List<int> intlist = new List<int>()
+//{
+//    1,3,4,6,7,8, 9,10,1,2,5,15,18
 
-foreach(Student stt in stud)
-{
-    stt.PrintFullName();
-}
+//};
 
+////Query syntax
+//var querysyntax = from obj in  intlist
+//                  where obj > 5
+//                  select obj;
 
+////method syntax
+//var methodsyntax = intlist.Where (obj => obj > 5);
 
-
-
-    
+//foreach(var method in methodsyntax)
+//{
+//    Console.WriteLine(method + " ");
+//}
+Console.ReadKey();
---
+++ Csharp-Introduction/QueryFunctions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Introduction
+{
+    internal class QueryFunctions
+    {
+        //ordering operators
+        public static void OrderingMethod()
+        {
+            List<int> intlist = new List<int>() { 10,45,30, 28,107,76,63,98};
+            Console.WriteLine("before sorting");
+            foreach(int i in intlist)
+            {
+                Console.WriteLine(i + " ");
+            }
+            
+            //method syntax
+            var data = intlist.Where(x => x > 30).OrderByDescending(x => x).ToList();
+
+            //query syntax
+            var datas = (from x in intlist
+                         where x > 30
+                         orderby x 
+                         select x).ToList();
+            Console.WriteLine("**********************");
+            Console.WriteLine("after sorting .............");
+            foreach(int i in datas)
+                Console.WriteLine(i + " ");
+        }
+    }
+}
